abf99795ed7277062d90c71c51253b2648f255d3
pengcheng.yang <806321554@qq.com>
2016-08-07 16:20:59 +0800

    test

--- dev_resource/js/home.js
+++ dev_resource/js/home.js
@@ -1,5 +1,7 @@
 var demo = document.querySelector(".demo");
-console.log(demo)
-demo.ondragend = function(){
-	alert("ok");
-};
+window.addEventListener("touchstart", function(e){
+	demo.innerHTML = "触屏开始：" + [e.touches.length, e.targetTouches.length, e.changedTouches.length];
+}, 0);
+window.addEventListener("touchend", function(e){
+	demo.innerHTML = "触屏结束：" + [e.touches.length, e.targetTouches.length, e.changedTouches.length];
+}, 0);
